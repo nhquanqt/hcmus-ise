ecbb5d9a04697799320b3ab3019e55f9da990a10
ttnhan18062000 <54936023+ttnhan18062000@users.noreply.github.com>
2021-01-04 14:06:40 +0700

    Revert "Revert "Add search-job.components to Client""
    This reverts commit eeaed4ccc1aed0f2b92458e9f46d698aa060462e.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import AddAccount from './components/add-account.component';
+import SearchJob from './components/search-job.components';
 
 class App extends Component {
     render () {
